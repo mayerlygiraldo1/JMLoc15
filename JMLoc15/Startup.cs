c8423be3772b7fad83f55ef65753dee0a2e01944
erickvelasco11 <erickvelasco11@gmail.com>
2016-06-14 23:29:48 -0500

    Files for Login

---
+++ JMLoc15/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JMLoc15.Startup))]
+namespace JMLoc15
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
